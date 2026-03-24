24e10d81212823d2ac2d0f02f32619ea449a7c4e
Andreea-Topita <andreea-loredana.topita@student.tuiasi.ro>
2026-03-24 16:32:32 +0200

    actualizare port 3307

--- models/database.py
+++ models/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URI = 'mysql+pymysql://root:si_2026@127.0.0.1:3306/crypto_db'
+DATABASE_URI = 'mysql+pymysql://root:si_2026@127.0.0.1:3307/crypto_db'
 
 engine = create_engine(DATABASE_URI, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
